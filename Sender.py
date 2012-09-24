5bf68d8a1ad8a604453dd70fe5098d6c3b69f6d8
Paul <Yeem>
2012-09-23 18:46:43 -0700

    Breaking down codes to multiple functions.

--- Sender.py
+++ Sender.py
@@ -8,8 +8,64 @@ import sys
 This is a skeleton sender class. Create a fantastic transport protocol here.
 '''
 class Sender(BasicSender.BasicSender):
+    
+    MAX_PACKET_SIZE = 1472
+    
+    currentMessageType = "start"
+    nextPacketData = "WHAT LE FUDGE"
+                
+    sentPackets = {}   # seqNum : packet
+        
+    startSize = self.MAX_PACKET_SIZE - sys.getsizeof("start") - 3*sys.getsizeof("|") - sys.getsizeof(0) - sys.getsizeof(0xffffffff)
+    dataSize = self.MAX_PACKET_SIZE - sys.getsizeof("data") - 3*sys.getsizeof("|") - sys.getsizeof(0) - sys.getsizeof(0xffffffff)        
+    endPacketSize = self.MAX_PACKET_SIZE - sys.getsizeof("end") - 3*sys.getsizeof("|") - sys.getsizeof(0) - sys.getsizeof(0xffffffff)
+        
     # Main sending loop.
     def start(self):
+        seqNum = 0      # The packet # that the RECEIVER is expecting
+        windowGap = 5   # This is how many "open" spaces we have for more packets
+        
+        packetList = {}  # Keep track of packets.  We may need to resend. 
+                            
+        
+                             
+        startPacket = self.make_packet("start", 0, self.infile.read(startSize))  
+        packetList[0] = startPacket
+        
+        ### TODO: NEED TO TAKE CARE OF THE CASE OF EMPTY FILE
+
+                                        
+        self.nextPacketData = self.infile.read(startSize)
+        
+        if self.nextPacketData == "":
+            self.currentMessageType = "end"
+        else:
+            self.currentMessageType = "data"
+        
+        # Main loop for sending.
+                 
+        
+                 
+                 
+        sendLoopCtrl = True
+        while(sendLoopCtrl):
+            
+            nextPacketData = 
+            self.send
+            
+            
+            
+            
+            
+            if windowGap == 0:
+                sendLoopCtrl = False
+         
+        
+        
+        
+        
+        
+        
         window = 5
         window_offset = 0
         pack_list  = []  
@@ -35,31 +91,44 @@ class Sender(BasicSender.BasicSender):
             seqno+=1
             pack_list.append(self.make_packet(msg_type, seqno, data))
             queue_packets+=1
+       
+    
+    # Send the packets to fill up the remaining window
+    def sendMyPackets(self, packetsToBeSent):
+        for packet in packetsToBeSent:
+            if (windowGap == 0):
+                break
+            else:
+                self.send(packet)
+                windowGap -= windowGap
+                packetsToBeACKed.append(packet)
+                packetsToBeSent.remove(packet)
+        self.recvMyACKs()        
         
-        for packet in pack_list:
-            self.send(packet)
+        
+    # Cumulatively recv's the ACK's from the receiver and processes them.
+    # If we don't know that all the packets have been received, e.g., we still need to loop, call sendMyPackets() 
+    def recvMyACKs(self):
+        try:
+            currentACK = self.receive(0.5)
+            while currentACK != None:  # If we get something, then check CHECKSUM
+                msg_typeself.split_packet()
+                
+                Checksum.validate_checksum()
+    
+    
+                currentACK = self.receive(0.5)
+
+        # No ACKs to be received.  Act accordingly.
+        except socket.timeout:
             
-        #wait for response
-        while(not (msg_type == 'end')):
-            response_packet = self.receive()
-            if Checksum.validate_checksum(response_packet):
-                next_seqno= response_packet.split("|")[1]
-                if(next_seqno == seqno + 1):
-                    data_size = 1472 - sys.getsizeof(0xffffffff) - sys.getsizeof(msg_type) - sys.getsizeof(seqno + 1)
-                    data = self.infile.read(data_size)
-                    if (data == ''):
-                        msg_type = 'end'
-                    seqno+=1
-                    packet = self.make_packet(msg_type, seqno, data)
-                    window_offset +=1
-                    pack_list.pop(0)
-                    pack_list.append(packet)
-                    self.send(packet)
-                else:
-                    #resend all of the packets from next_seqno to end of window??  not sure this will solve every case
-                    raise NotImplementedError
-                    
-        
+    # How specific do we have to be?  Check if the fields are numeric?            
+    def checkACKPacket(self, packet):
+        splitPacket = packet.split("|")
+        if len(splitPacket) != 3:
+            return False
+        elif Checksum.validate_checksum(packet):
+            return True
 
 '''
 This will be run if you run this script from the command line. You should not
